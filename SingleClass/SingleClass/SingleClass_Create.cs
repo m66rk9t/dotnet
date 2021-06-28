6b4e174ff9825bff96dd5b3ad1e93285c2d10045
m66rk9t <m66rk9t@gmail.com>
2021-06-28 15:29:01 +0800

    单例模式

---
+++ SingleClass/SingleClass/SingleClass_Create.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleClass
+{
+    public class SingleClass_Create
+    {
+        //声明私有静态字段存储唯一实例
+        private static SingleClass_Create sc;
+
+        //私有默认构造器构造唯一实例
+        private SingleClass_Create() 
+        {
+            Console.WriteLine("已创建唯一实例");
+        }
+
+        //公共静态方法创建唯一实例
+        public static SingleClass_Create CreateSC()
+        {
+            //判断是否已创建实例
+            if(sc==null)
+            {
+                sc = new SingleClass_Create();
+            }
+
+            //返回值
+            return sc;
+        }
+    }
+}
